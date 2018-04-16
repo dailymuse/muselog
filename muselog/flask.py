00e09cd3be82239ee82300fec66860293680377d
andela-sjames <samuel.james@andela.com>
2018-04-16 14:45:18 +0100

    modify flask logger for graylog enabled app

--- muselog/flask.py
+++ muselog/flask.py
@@ -8,35 +8,32 @@ import traceback
 logger = logging.getLogger(__name__)
 
 # http://flask.pocoo.org/docs/0.12/api/#flask.Flask.after_request
-
-# usage with @app.after_request(response)
 def log_after_request(response):
-    """ Logging after every request. """
-    # This avoids the duplication of registry in the log,
-    # since that 500 is already logged via @app.errorhandler.
-    if response.status_code != 500:
-        ts = strftime('[%Y-%b-%d %H:%M]')
-        tb = traceback.format_exc()
-        logger.error('%s %s %s %s %s %s %s',
-                      ts,
-                      request.remote_addr,
-                      request.method,
-                      request.scheme,
-                      request.full_path,
-                      response.status,
-                      tb)
-    return response
+    """Logging after every flask request with extra context for use w/ Graylog-enabled apps.
+    
+    Usage:
+    @app.after_request
+    log_after_request(response)
 
-# usage with @app.errorhandler(Exception)
-def log_exceptions(e):
-    """ Logging after every Exception. """
+    """
+
+    if response.status_code < 400:
+        log_method = logger.info
+    elif response.status_code < 500:
+        log_method = logger.warning
+    else:
+        log_method = logger.error
+
     ts = strftime('[%Y-%b-%d %H:%M]')
     tb = traceback.format_exc()
-    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
-                  ts,
-                  request.remote_addr,
-                  request.method,
-                  request.scheme,
-                  request.full_path,
-                  tb)
-    return "Internal Server Error", 500
+    request_summary = "{}-{}-{}".format(request.method, request.query_string, request.remote_addr)
+
+    log_method("%d %s", response.status, request_summary,
+               extra={"request_method": request.method,
+                      "request_path": request.full_path,
+                      "request_query": request.query_string,
+                      "response_status": response.status,
+                      "request_remote_ip": request.remote_addr,
+                      "request_summary": request_summary,
+                      "traceback": tb})
+    return response
