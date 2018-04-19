2a838cd8caef01603ae3bdc0395b36f03444f0ff
andela-sjames <samuel.james@andela.com>
2018-04-19 13:03:36 +0100

    refactor to reflect flask logging pattern

--- muselog/__init__.py
+++ muselog/__init__.py
@@ -2,13 +2,36 @@
 
 import logging
 import os
-from typing import Mapping, Optional, Union
+from typing import Mapping, Optional, Union, Any
 
 from pygelf import GelfUdpHandler, GelfTlsHandler
 
 
 #: Format to use
 DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s"
+
+def add_gelf_handler(root_logger):
+    if "GRAYLOG_HOST" in os.environ:
+        common_opts = dict(
+            host=os.environ["GRAYLOG_HOST"],
+            debug=bool(os.environ.get("GRAYLOG_DEBUG", True)),
+            include_extra_fields=True,
+            _app_name=os.environ["GRAYLOG_APP_NAME"],
+            _env=os.environ["GRAYLOG_ENV"]
+        )
+        handler_type = os.environ.get("GRAYLOG_HANDLER_TYPE", "tls").lower()
+        if handler_type == "udp":
+            gelf_handler = GelfUdpHandler(**common_opts,
+                                          port=int(os.environ.get("GRAYLOG_UDP_PORT", 12201)),
+                                          chunk_size=int(os.environ.get("GRAYLOG_UDP_CHUNK_SIZE", 1300)),
+                                          compress=bool(os.environ.get("GRAYLOG_UDP_COMPRESS", True)))
+        elif handler_type == "tls":
+            gelf_handler = GelfTlsHandler(**common_opts,
+                                          port=int(os.environ.get("GRAYLOG_TLS_PORT", 12201)),
+                                          timeout=float(os.environ.get("GRAYLOG_TLS_TIMEOUT_SECS", 0.3)))
+        else:
+            raise ValueError("Graylog handler type '{}' not recognized. Valid types are 'udp' and 'tls'.".format(handler_type))
+        root_logger.addHandler(gelf_handler)
 
 
 def setup_logging(root_log_level: Optional[str] = None,
@@ -31,26 +54,7 @@ def setup_logging(root_log_level: Optional[str] = None,
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(logging.Formatter(fmt=console_handler_format or DEFAULT_LOG_FORMAT))
         root_logger.addHandler(console_handler)
+        
 
     # Add GELF handler if GELF is enabled
-    if "GRAYLOG_HOST" in os.environ:
-        common_opts = dict(
-            host=os.environ["GRAYLOG_HOST"],
-            debug=bool(os.environ.get("GRAYLOG_DEBUG", True)),
-            include_extra_fields=True,
-            _app_name=os.environ["GRAYLOG_APP_NAME"],
-            _env=os.environ["GRAYLOG_ENV"]
-        )
-        handler_type = os.environ.get("GRAYLOG_HANDLER_TYPE", "tls").lower()
-        if handler_type == "udp":
-            gelf_handler = GelfUdpHandler(**common_opts,
-                                          port=int(os.environ.get("GRAYLOG_UDP_PORT", 12201)),
-                                          chunk_size=int(os.environ.get("GRAYLOG_UDP_CHUNK_SIZE", 1300)),
-                                          compress=bool(os.environ.get("GRAYLOG_UDP_COMPRESS", True)))
-        elif handler_type == "tls":
-            gelf_handler = GelfTlsHandler(**common_opts,
-                                          port=int(os.environ.get("GRAYLOG_TLS_PORT", 12201)),
-                                          timeout=float(os.environ.get("GRAYLOG_TLS_TIMEOUT_SECS", 0.3)))
-        else:
-            raise ValueError("Graylog handler type '{}' not recognized. Valid types are 'udp' and 'tls'.".format(handler_type))
-        root_logger.addHandler(gelf_handler)
+    add_gelf_handler(root_logger)
--- muselog/flask.py
+++ muselog/flask.py
@@ -2,41 +2,49 @@
 Custom logger for user auth service in python flask 
 """
 
+import sys
 import logging
-import traceback
 
 from time import strftime
-
+from muselog import add_gelf_handler
 
 logger = logging.getLogger(__name__)
 
 # http://flask.pocoo.org/docs/0.12/api/#flask.Flask.after_request
-def log_request(request, response):
+def log_request(app, request, response):
     """Logging after every flask request with extra context for use w/ Graylog-enabled apps.
     
     Usage:
     @app.after_request
     def after_request(response):
-        return muselog.flask.log_request(request, response)
+        return muselog.flask.log_request(app, request, response)
 
     """
 
+    root_logger = app.logger
+    root_logger.setLevel(logging.INFO)
+    add_gelf_handler(root_logger)
+    root_logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+    # http://flask.pocoo.org/docs/0.12/quickstart/#logging
     if response.status_code < 400:
-        log_method = logger.info
+        log_method = app.logger.info
     elif response.status_code < 500:
-        log_method = logger.warning
+        log_method = app.logger.warning
     else:
-        log_method = logger.error
+        log_method = app.logger.error
 
     ts = strftime('[%Y-%b-%d %H:%M]')
-    tb = traceback.format_exc()
-    request_summary = "{}-{}-{}".format(request.method, request.query_string, request.remote_addr)
+    resquest_query = request.query_string.decode("utf-8")
+    request_summary = "{0} {1} {2}".format(request.method, resquest_query, request.remote_addr)
 
-    log_method("%d %s %s", response.status_code, request_summary, ts,
-               extra={"request_method": request.method,
-                      "request_path": request.full_path,
-                      "request_query": request.query_string,
-                      "response_status": response.status,
-                      "request_remote_ip": request.remote_addr,
-                      "request_summary": request_summary})
+
+    data={"request_method": request.method,
+          "request_path": request.full_path,
+          "request_query": resquest_query,
+          "response_status": response.status,
+          "request_remote_ip": request.remote_addr,
+          "request_summary": request_summary}
+  
+    log_method("%d %s %s", response.status_code, request_summary, ts, extra=data)
     return response
