3b3e28cacd84903b087d3cf7fe1c9e2390aa42a6
Dan Cadden <Shikkic@gmail.com>
2019-03-18 18:44:50 -0400

    remove pinned versions of dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ VERSION = "1.6.0"
 
 install_requires = [
     "pygelf>=0.4.1",
-    "JSON-log-formatter==0.2.0",
-    "ddtrace==0.22.0" # This is the minimum version allowed as trace helpers weren't added until 0.22.0
+    "JSON-log-formatter>=0.2.0",
+    "ddtrace>=0.22.0" # This is the minimum version allowed as trace helpers weren't added until 0.22.0
 ]
 
 setup(
