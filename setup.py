be0f68f08fde9338285dff319980638d7fb64ada
Ian Axelrod <ian@themuse.com>
2019-04-03 15:37:02 -0400

    Update version to 1.7.0
    PLAT-1823

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-VERSION = "1.6.2"
+VERSION = "1.7.0"
 
 install_requires = [
     "pygelf>=0.4.1",
     "JSON-log-formatter>=0.2.0",
-    "ddtrace>=0.22.0" # This is the minimum version allowed as trace helpers weren't added until 0.22.0
+    "ddtrace>=0.22.0"  # This is the minimum version allowed as trace helpers weren't added until 0.22.0
 ]
 
 setup(
