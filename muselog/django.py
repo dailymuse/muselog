40dab1b309bafe0f55fbfc54fc8e3d56f67450ee
Ian Axelrod <ian@themuse.com>
2019-04-03 23:44:37 -0400

    Use correct attributes for django & tornado
    PLAT-1823

--- muselog/django.py
+++ muselog/django.py
@@ -1,10 +1,12 @@
+"""Middleware to log django request information."""
+
 import logging
-from datetime import datetime
+import time
 
 logger = logging.getLogger(__name__)
 
 
-class MuseDjangoRequestLoggingMiddleware(object):
+class MuseDjangoRequestLoggingMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -17,28 +19,34 @@ class MuseDjangoRequestLoggingMiddleware(object):
         return response
 
     def process_request(self, request):
-        request.started_at = datetime.utcnow().timestamp()
+        request.started_at = time.time()
 
     def process_response(self, request, response):
         response_status = response.status_code
-        request_time = 1000.0 * datetime.utcnow().timestamp() - request.started_at
+        request_time = 1000 * (time.time() - request.started_at)
         request_ip = self.get_client_ip(request)
         request_summary = f"{request.method} {request.get_full_path()} ({request_ip})"
 
-        self.logger.debug(
+        if response_status < 400:
+            log_method = self.logger.info
+        elif response_status < 500:
+            log_method = self.logger.warning
+        else:
+            log_method = self.logger.error
+
+        extra = {
+            "duration": int(request_time * 1000000),
+            **self._derive_network_attrs(request, response),
+            **self._derive_http_attrs(request, response),
+            **self._derive_usr_attrs(request)
+        }
+
+        log_method(
             "%d %s %.2fms",
             response_status,
             request_summary,
             request_time,
-            extra={
-                "request_method": request.method,
-                "request_path": request.path,
-                "request_query": request.GET.urlencode(),
-                "response_status": response_status,
-                "request_duration": request_time,
-                "request_remote_ip": request_ip,
-                "request_summary": request_summary
-            }
+            extra=extra
         )
 
     def get_client_ip(self, request):
@@ -51,3 +59,51 @@ class MuseDjangoRequestLoggingMiddleware(object):
 
     def set_logger(self, logger):
         self.logger = logger
+
+    def _derive_network_attrs(self, request, response):
+        result = {
+            "network.client.ip": self.get_client_ip(request)
+            # "network.client.port": ....
+            # There is no (public) interface to get the client's port. Even if one
+            # existed, that port could be a load balancer port, whereas the remote ip
+            # is extracted from X-Forwarded-For. This would be misleading, so we do
+            # not include the client port.
+
+            # "network.destination.ip": ....
+            # The destination ip and port is not useful in this context, so
+            # we do not include it
+        }
+
+        # This is iffy, as it's unclear what network layer 'bytes_read' and 'bytes_written' refers to
+        # Still, the info is too useful to ignore, and the error is small.
+        result["network.bytes_read"] = int(request.META.get("CONTENT_LENGTH") or 0)
+        result["network.bytes_written"] = response.tell()
+
+        return result
+
+    def _derive_http_attrs(self, request, response):
+        headers = request.META
+        result = {
+            "http.url": request.get_raw_uri(),
+            "http.method": request.method,
+            "http.status_code": response.status_code,
+        }
+        request_id = headers.get("HTTP_X_REQUEST_ID") or headers.get("HTTP_X_AMZN_TRACE_ID")
+        if request_id:
+            result["http.request_id"] = request_id
+        if "HTTP_REFERER" in headers:
+            result["http.referer"] = headers["HTTP_REFERER"]
+        if "HTTP_USER_AGENT" in headers:
+            result["http.useragent"] = headers["HTTP_USER_AGENT"]
+
+        return result
+
+    def _derive_usr_attrs(self, request):
+        result = dict()
+        if not hasattr(request, "user"):
+            return result
+        user = request.user
+        if user.is_authenticated:
+            result["usr.id"] = user.id
+
+        return result
--- muselog/tornado.py
+++ muselog/tornado.py
@@ -1,17 +1,71 @@
-"""
-Helpers to log tornado request information.
-"""
+"""Helpers to log tornado request information."""
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-def log_request(handler):
-    """Log the request information with extra context for use w/ Graylog-enabled apps."""
 
+def _derive_network_attrs(handler, request):
+    result = {
+        "network.client.ip": request.remote_ip,
+        # "network.client.port": ....
+        # There is no (public) interface to get the client's port. Even if one
+        # existed, that port could be a load balancer port, whereas the remote ip
+        # is extracted from X-Forwarded-For. This would be misleading, so we do
+        # not include the client port.
+
+        # "network.destination.ip": ....
+        # The destination ip and port is not useful in this context, so
+        # we do not include it
+    }
+
+    # This is iffy, as it's unclear what network layer 'bytes_read' and 'bytes_written' refers to
+    # Still, the info is too useful to ignore, and the error is small.
+    result["network.bytes_read"] = int(request.headers.get("Content-Length") or 0)
+
+    # Tornado does not save the response object, and muselog is not in the business
+    # of providing middleware at the moment. Will need to re-architect the tornado
+    # setup to work as middleware.
+    # result["network.bytes_written"] = ?
+
+    return result
+
+
+def _derive_http_attrs(handler, request):
+    headers = request.headers
+    result = {
+        "http.url": request.full_url(),
+        "http.method": request.method,
+        "http.status_code": handler.get_status(),
+    }
+    request_id = headers.get("X-Request-Id") or headers.get("X-Amzn-Trace-Id")
+    if request_id:
+        result["http.request_id"] = request_id
+    if "Referer" in headers:
+        result["http.referer"] = headers["Referer"]
+    if "User-Agent" in headers:
+        result["http.useragent"] = headers["User-Agent"]
+
+    return result
+
+
+def _derive_usr_attrs(handler):
+    result = dict()
+    user = handler.current_user
+    if user:
+        if hasattr(user, "id"):
+            result["usr.id"] = str(user.id)
+        elif isinstance(user, dict) and "id" in user:
+            result["usr.id"] = str(user["id"])
+        elif isinstance(user, str) or isinstance(user, int):
+            result["usr.id"] = str(user)
+    return result
+
+
+def log_request(handler):
+    """Log the request information with extra context."""
+    request = handler.request
     response_status = handler.get_status()
-    request_time = 1000.0 * handler.request.request_time()
-    request_summary = handler._request_summary()
 
     if response_status < 400:
         log_method = logger.info
@@ -20,11 +74,18 @@ def log_request(handler):
     else:
         log_method = logger.error
 
-    log_method("%d %s %.2fms", response_status, request_summary, request_time,
-               extra={"request_method": handler.request.method,
-                      "request_path": handler.request.path,
-                      "request_query": handler.request.query,
-                      "response_status": response_status,
-                      "request_duration": request_time,
-                      "request_remote_ip": handler.request.remote_ip,
-                      "request_summary": request_summary})
+    request_time = 1000.0 * request.request_time()
+    extra = {
+        "duration": int(request_time * 1000000),
+        **_derive_network_attrs(handler, request),
+        **_derive_http_attrs(handler, request),
+        **_derive_usr_attrs(handler)
+    }
+
+    log_method(
+        "%d %s %.2fms",
+        response_status,
+        handler._request_summary(),
+        request_time,
+        extra=extra
+    )
